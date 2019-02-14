4dc7e4662dd18b91a48bbeacdff850f1630b2d8c
CM Student <cmstudent@UHWO-ACM-C-06.local>
2019-02-14 10:30:26 -1000

    Completed Inclass Week 6

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -3,3 +3,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+var fruitNames: [String]
+
+//Assign fruitNames with 3 of your favorite fruits
+fruitNames = ["Kiwi", "Apples", "Watermelon"]
+
+//Part 4
+print(fruitNames)
+
+//Part 5 (string interpolation, this is not an iteration)
+print("I like to eat \(fruitNames)")
+
+//Part 6
+for name in fruitNames
+{
+print("I like to eat " + name)
+}
+
+//Part 7 #2
+//Print "name" at index 1 in fruitNames
+print(fruitNames[1])
+print(fruitNames[0])
